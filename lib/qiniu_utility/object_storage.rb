fb3c93ac398918647a4e90706fb1b62fa11d67e0
Andersen Fan <as181920@gmail.com>
2019-06-28 17:18:22 +0800

    Fix fetch_from_url: use urlsafe_encode64

--- lib/qiniu_utility/object_storage.rb
+++ lib/qiniu_utility/object_storage.rb
@@ -14,7 +14,7 @@ module QiniuUtility
 
     def fetch_from_url(original_url, bucket)
       QiniuUtility.logger.info "QiniuUtility::ObjectStorage fetch_from_url reqt: #{original_url}"
-      api_url = "http://iovip.qbox.me/fetch/#{Base64.strict_encode64(original_url)}/to/#{Base64.strict_encode64(bucket)}"
+      api_url = "http://iovip.qbox.me/fetch/#{Base64.urlsafe_encode64(original_url)}/to/#{Base64.urlsafe_encode64(bucket)}"
       resp = Faraday.post api_url, {}, {Authorization: "QBox #{generate_access_token(api_url)}"}
       QiniuUtility.logger.info "QiniuUtility::ObjectStorage fetch_from_url resp(#{resp.status}): #{resp.body}"
       JSON.load(resp.body)["key"]
