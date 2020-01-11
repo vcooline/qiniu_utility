2cd56118859a18909a1846430a06e9551a33d1a1
Andersen Fan <as181920@gmail.com>
2020-01-11 14:46:39 +0800

    Add change_meta_data

--- lib/qiniu_utility/object_storage.rb
+++ lib/qiniu_utility/object_storage.rb
@@ -12,6 +12,16 @@ module QiniuUtility
       Qiniu::Auth.generate_acctoken(url)
     end
 
+    def stat(bucket, key)
+      Qiniu.stat(bucket, key)
+    end
+
+    def change_meta_data(bucket, key, meta_key, meta_value)
+      encoded_entry_uri = Base64.urlsafe_encode64 [bucket, key].join(":")
+      encoded_meta_value = Base64.urlsafe_encode64 meta_value
+      Qiniu::HTTP.management_post "https://rs.qbox.me/chgm/#{encoded_entry_uri}/x-qn-meta-#{meta_key}/#{encoded_meta_value}"
+    end
+
     def fetch_from_url(original_url, bucket)
       QiniuUtility.logger.info "QiniuUtility::ObjectStorage fetch_from_url reqt: #{original_url}"
       api_url = "http://iovip.qbox.me/fetch/#{Base64.urlsafe_encode64(original_url)}/to/#{Base64.urlsafe_encode64(bucket)}"
