333ed06bc062462ee5e27e4e69402f9832eb12b8
Andersen Fan <as181920@gmail.com>
2023-04-27 16:16:26 +0800

    Refactor object storage list with pagination by marker

--- lib/qiniu_utility/object_storage.rb
+++ lib/qiniu_utility/object_storage.rb
@@ -27,15 +27,17 @@ module QiniuUtility
     def fetch_from_url(original_url, bucket, download_url: "http://iovip.qbox.me")
       QiniuUtility.logger.info "QiniuUtility::ObjectStorage fetch_from_url reqt: #{original_url}"
       api_url = "#{download_url}/fetch/#{Base64.urlsafe_encode64(original_url)}/to/#{Base64.urlsafe_encode64(bucket)}"
-      resp = Faraday.post api_url, {}, {Authorization: "QBox #{generate_access_token(api_url)}"}
+      resp = Faraday.post api_url, {}, { Authorization: "QBox #{generate_access_token(api_url)}" }
       QiniuUtility.logger.info "QiniuUtility::ObjectStorage fetch_from_url resp(#{resp.status}): #{resp.body}"
-      JSON.load(resp.body)["key"]
+      JSON.parse(resp.body)["key"]
     end
 
-    def list(bucket, limit=100)
-     code, result, response_headers, s, d = Qiniu::Storage.list(Qiniu::Storage::ListPolicy.new(bucket, limit))
-     QiniuUtility.logger.info "QiniuUtility::ObjectStorage list #{bucket} resp(#{code}): #{result['error'] || {items_count: result["items"].count}.to_json}; #{response_headers.to_json}; #{s.to_json}; #{d.to_json}"
-     result["items"]
+    def list(bucket, limit: 100, marker: nil)
+      list_policy = Qiniu::Storage::ListPolicy.new(bucket, limit)
+        .tap { |policy| policy.marker = marker if marker.present? }
+      code, result, _response_headers, _s, d = Qiniu::Storage.list(list_policy)
+      QiniuUtility.logger.info "#{self.class.name} list #{bucket} resp(#{code}): #{result['error'] || { items_count: result['items'].count }.to_json}; #{d.to_json}"
+      result
     end
 
     def change_file_type(bucket, key, type, base_url: "http://rs.qiniu.com")
@@ -55,6 +57,5 @@ module QiniuUtility
     def authenticate_callback_request(auth_str, url, body)
       Qiniu::Auth.authenticate_callback_request(auth_str.to_s, url.to_s, body.to_s)
     end
-
   end
 end
